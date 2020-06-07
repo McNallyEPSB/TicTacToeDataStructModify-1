f125c4a55025d8393d7cc190257c5dd68737277c
Lance <lance.pedersen@epsb.ca>
2020-06-07 21:00:25 +0000

    2d array from comma separated to properly array within array

--- main.js
+++ main.js
@@ -2,12 +2,15 @@
 
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext ("2d");
-c.width = 600;
-c.height = 600;
+c.width = 450;
+c.height = 450;
 
 let numRows = 3;
 let numCols = 3;
-let array = [numRows, numCols];
+let gameBoard = new Array(numRows);
+for (let c = 0; c < gameBoard.length; c++) { 
+    gameBoard[c] = new Array(numCols); 
+}
 
 let spacingHor = c.width/numCols;
 let spacingVert = c.height/numRows;
@@ -22,42 +25,34 @@ ctx.strokeStyle="black";
 ctx.lineWidth = "1";
 
 
-let initBoard = function(){
+let initBoard = function(){//initialize the game state in the data structure behind the user interface - currently all to 0 (not onwned by first player :1 or second player:2)
   for (let r = 0; r < numRows; r++){
       for (let c = 0; c < numCols; c++){
-        array[r,c] = 0;
-        console.log(array[r,c]);
+        gameBoard[r][c] = 0;
+        console.log(gameBoard[r][c]);
       }
   }
 }
 
-
-
-let drawBoard = function(){
+let drawBoard = function(){//draw the user interface in the canvas to represent state stored in the board data structure
   for (let r = 0; r < numRows; r++){
       for (let c = 0; c < numCols; c++){
-        if (array[r,c] == 0)
+        if (gameBoard[r][c] == 0)
           ctx.fillStyle = 'white';
-        else if (array[r,c] == 1)//is this because current player = 1?
+        else if (gameBoard[r][c]== 1)//is this because current player = 1?
           ctx.fillStyle = 'red';
-        else if (array[r,c] == 2)
-          ctx.fillStyle = 'black';
+        else if (gameBoard[r][c] == 2)
+          ctx.fillStyle = 'blue';
         
       
         ctx.strokeRect(c*spacingHor, r*spacingVert, xFill, yFill);
-        ctx.fillRect(c*spacingHor, r*spacingVert, xFill, yFill);        
-        
+        ctx.fillRect(c*spacingHor, r*spacingVert, xFill, yFill);                
       }
   }
 }
 
-
-
-initBoard();
-drawBoard();
-
 //https://stackoverflow.com/questions/1114465/getting-mouse-location-in-canvas
-  function getMousePos(canvas, evt) {
+  function getMousePos(canvas, evt) {//get the mouseposition in relation to the canvas - css must have canvas position set to relative
     var rect = canvas.getBoundingClientRect();
     return {
       x: evt.clientX - rect.left,
@@ -65,7 +60,7 @@ drawBoard();
     };
   }
  
-   function getCanvasRC(canvas, evt) {
+   function getCanvasRC(canvas, evt) {//converts the mouse position from x,y relative to canvas into which row and column
     var rect = canvas.getBoundingClientRect();
     return {
       c: Math.floor((evt.clientX - rect.left)/c.width*numCols),
@@ -80,20 +75,18 @@ drawBoard();
 */
  myCanvas.addEventListener('click', function(evt) {
     var canvasRC = getCanvasRC(myCanvas, evt);
-    var currentX = canvasRC.r;
-    var currentY = canvasRC.c;
-
-  if (array[canvasRC.r, canvasRC.c]==0){
+    
+  if (gameBoard[canvasRC.r][canvasRC.c]==0){//if a row and column is unclaimed (gamestate 0) then it is claimed by whoever's turn it is
     console.log('Row: ' + (canvasRC.r) + ' , Col: ' + (canvasRC.c));
-    array[currentX, currentY] = currentPlayer;
+    gameBoard[canvasRC.r][canvasRC.c] = currentPlayer;
     
     for (let r = 0; r < numRows; r++){
       for (let c = 0; c < numCols; c++){
-        console.log(array[r, c]);    
+        console.log(gameBoard[r][c]);    
       }
     }
   drawBoard();
-  if (currentPlayer == 1)
+  if (currentPlayer == 1)//flip flop between player turns
       currentPlayer = 2;
     else 
       currentPlayer = 1;
@@ -101,3 +94,7 @@ drawBoard();
 }, false);
 
 
+//run the game
+initBoard();
+drawBoard();
+//need to build 
